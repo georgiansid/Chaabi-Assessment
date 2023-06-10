d17450cded78813185b52452f07ba7b12b728b62
Siddhant Lamba <87963421+georgiansid@users.noreply.github.com>
2023-06-10 23:51:11 +0530

    Delete Question10.py

--- Question10.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-def get_previous_date(date, n):
-    myDate = datetime.strptime(date, '%y-%m-%d')
-    prevDate = myDate - timedelta(days=n)
-    prevDateStr = prevDate.strftime('%y-%m-%d')
-    return prevDateStr
-date = '16-12-10'
-prevTime = 11
-print(get_previous_date(date,prevTime))
